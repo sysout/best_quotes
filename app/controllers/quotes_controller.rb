25149a9e38c77da709a06183ac7b4063df2edfc2
Xiong Xiong <xiongx8404@gmail.com>
2017-03-20 15:47:26 -0700

    Finish chapter 5

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,31 @@
 class QuotesController < Rulers::Controller
+
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
   def a_quote
     render :a_quote, :noun => :winking
+  end
+
+  def quote_1
+    quote_1 = FileModel.find(1)
+    render :quote, :obj => quote_1
+  end
+
+  def new_quote
+    attrs = {
+        "submitter" => "Jeff",
+        "quote" => "A penny saved is a penny earned.",
+        "attribution" => "Ben Franklin"
+    }
+    m = FileModel.create attrs
+    render :quote, obj: m
+  end
+
+  def update_quote
+    return "" if env["REQUEST_METHOD"]!="POST"
+    "OK"
   end
 end
