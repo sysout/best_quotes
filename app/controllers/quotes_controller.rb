acebd8d2518e95fb7421e98e80d03612b83f4f86
Xiong Xiong <xiongx8404@gmail.com>
2017-03-20 17:37:28 -0700

    Finish chapter 6

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,17 +1,23 @@
 class QuotesController < Rulers::Controller
 
   def index
-    quotes = FileModel.all
-    render :index, :quotes => quotes
+    @quotes = FileModel.all
+    # render_response :index, :quotes => quotes
+  end
+
+  def show
+    @obj = FileModel.find(params["id"])
+    @ua = request.user_agent
+    # render_response :quote, :obj => quote, :ua => ua
   end
 
   def a_quote
-    render :a_quote, :noun => :winking
+    render_response :a_quote, :noun => :winking
   end
 
   def quote_1
     quote_1 = FileModel.find(1)
-    render :quote, :obj => quote_1
+    render_response :show, :obj => quote_1
   end
 
   def new_quote
@@ -21,7 +27,7 @@ class QuotesController < Rulers::Controller
         "attribution" => "Ben Franklin"
     }
     m = FileModel.create attrs
-    render :quote, obj: m
+    render_response :show, obj: m
   end
 
   def update_quote
